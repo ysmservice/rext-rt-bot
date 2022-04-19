787a78ef6087fe3537dd96ae0cdf06b7ff9029f5
mc_fdc <45692264+tuna2134@users.noreply.github.com>
2022-04-20 08:00:49 +0900

    Update reprypt.py

--- cogs/reprypt.py
+++ cogs/reprypt.py
@@ -2,14 +2,14 @@
 
 from time import time
 from discord.ext import commands
-from rtlib import Cog
+from rtlib import Cog, RT
 import reprypt
 from discord import app_commands
 import discord
 
 
 class Reprypt(Cog):
-    def __init__(self, bot):
+    def __init__(self, bot: RT):
         self.bot = bot
 
     @commands.group(
@@ -20,21 +20,21 @@ class Reprypt(Cog):
         if not ctx.invoked_subcommand:
             await ctx.reply(
                 {"ja": "使い方が違います。",
-                 "en": "..."}
+                 "en": "The usage is different."}
             )
 
-    @reprypt_.command(aliases=["en"])
-    @app_commands.describe(key="The password required for decryption.")
-    @app_commands.describe(content="The text to be encrypted.")
+    @reprypt_.command(aliases=("en"))
+    @app_commands.describe(key="The password required for decryption.",
+                           content="The text to be encrypted.")
     async def encrypt(self, ctx, key: str, *, content: str):
         result = reprypt.encrypt(content, key)
         await ctx.reply(
-            f"```\n{result}\n```", replace_language=False,
-            allowed_mentions=discord.AllowedMentions.none()
+            f"```\n{result}\n```", allowed_mentions=discord.AllowedMentions.none()
         )
 
-    @reprypt_.command(aliases=["de"])
-    async def decrypt(self, ctx, key, *, content):
+    @reprypt_.command(aliases=("de"))
+    @app_commands.describe(key="The password used for encryption.", content="The encrypted text to be decrypted.")
+    async def decrypt(self, ctx, key: str, *, content: str):
         result = reprypt.decrypt(content, key)
         await ctx.reply(
             f"```\n{result}\n```", replace_language=False,
@@ -44,17 +44,17 @@ class Reprypt(Cog):
     Cog.HelpCommand(reprypt_) \
         .set_description(ja="Repryptを使用して文章を暗号化/復号化します。", en="Encryption/Decryption by Reprypt.") \
         .update_headline(ja="Repryptを使用して文章を暗号化/復号化します。") \
-        .add_sub(Cog.HelpCommand(encrypt) \
-                    .set_description(ja="指定された文章を暗号化します。", en="Encrypts the specified text.") \
+        .add_sub(Cog.HelpCommand(encrypt)
+                    .set_description(ja="指定された文章を暗号化します。", en="Encrypts the specified text.")
                     .add_args("key", "str", ja="復号時に必要となるパスワードです。",
-                              en="The password required for decryption.") \
+                              en="The password required for decryption.")
                     .add_args("content", "str", ja="暗号化する文章です。",
                               en="The text to be encrypted.")
                 )
-        .add_sub(Cog.HelpCommand(decrypt) \
-                    .set_description(ja="Repryptで暗号化された文章を復号化します。", en="Decrypts the text encrypted by Reprypt.") \
+        .add_sub(Cog.HelpCommand(decrypt)
+                    .set_description(ja="Repryptで暗号化された文章を復号化します。", en="Decrypts the text encrypted by Reprypt.")
                     .add_args("key", "str", ja="暗号化する時に使ったパスワードです。",
-                              en="The password used for encryption.") \
+                              en="The password used for encryption.")
                     .add_args("content", "str", ja="復号したい暗号化された文章です。",
                               en="The encrypted text to be decrypted.")
                 )
