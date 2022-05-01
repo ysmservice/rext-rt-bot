3f9fddbe7125d9b10f259af0a6b49b4dddc5012f
mc_fdc <45692264+tuna2134@users.noreply.github.com>
2022-05-01 20:36:33 +0900

    Update entertainment.py

--- cogs/entertainment/entertainment.py
+++ cogs/entertainment/entertainment.py
@@ -26,7 +26,7 @@ class Entertainment(Cog):
             await ctx.send(embed=embed)
             
     HelpCommand(minecraft) \
-        .set_description("マイクラユーザー検索")
+        .set_description(ja="マイクラユーザー検索", en="minecraft user search)
         .add_arg("user", "str", None,
                  ja="マイクラのユーザ名", en="Minecraft user name")
         .update_headline(ja="マイクラのユーザー検索をします", en="Search minecraft user")
