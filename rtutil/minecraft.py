f41a928598ef90f216085530945f0b376b9ab2df
mc_fdc <45692264+tuna2134@users.noreply.github.com>
2022-05-01 19:14:48 +0900

    Create minecraft.py

---
+++ rtutil/minecraft.py
@@ -0,0 +1,34 @@
+from typing import Dict
+from aiohttp import ClientSession
+
+
+async def search(user: str):
+    async with ClientSession() as session:
+        async with session.get(
+            "https://api.mojang.com/users/profiles/minecraft/{}".format(user)
+        ) as r:
+            if r.status == 204:
+                raise NotFound("I can't found that user")
+            
+                
+class NotFound(Exception):
+    pass
+
+
+class Data:
+    def __init__(self, data: Dict[str, str]):
+        self.data = data
+        
+    @property
+    def name(self) -> str:
+        return self.data["name"]
+    
+    @property
+    def id(self) -> str:
+        return self.data["id"]
+    
+    @property
+    def skin(self) -> str:
+        return "https://minecraft.tools/en/skins/getskin.php?name={}".format(
+            self.name
+        )
