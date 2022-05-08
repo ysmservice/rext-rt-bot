1ea0c077474b6c12f91a77c9ed2e06ac9a463569
mc_fdc <45692264+tuna2134@users.noreply.github.com>
2022-05-08 09:03:25 +0900

    Update userinfo.py

--- cogs/individual/userinfo.py
+++ cogs/individual/userinfo.py
@@ -11,7 +11,18 @@ class Userinfo(Cog):
     @commands.command()
     async def userinfo(self, ctx, *, id: int=None):
         user = await self.bot.search_user(id)
-        Cog.Embed(title="")
+        if user.public_flags.hypesquad_bravery:
+            hypesquad = "<:HypeSquad_Bravery:876337861572579350>"
+        elif user.public_flags.hypesquad_brilliance:
+            hypesquad = "<:HypeSquad_Brilliance:876337861643882506>"
+        elif user.public_flags.hypesquad_balance:
+            hypesquad = "<:HypeSquad_Balance:876337714679676968>"
+        else:
+            hypesquad = ""
+        Cog.Embed(
+            title=user,
+            description=hypesquad
+        )
  
 
 async def setup(bot):
