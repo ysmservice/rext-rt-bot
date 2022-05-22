e737ce2f9e41054e6fe4bb6357906749b3794a23
tasuren <tasuren_0618@yahoo.co.jp>
2022-05-22 16:58:50 +0900

    update: 権限表示コマンドを実装した。

--- cogs/server-tool/__init__.py
+++ cogs/server-tool/__init__.py
@@ -5,6 +5,8 @@ import discord
 
 from core import Cog, RT, t
 
+from data import EMOJIS, PERMISSION_TEXTS
+
 
 FSPARENT = "server-tool"
 
@@ -35,6 +37,43 @@ class ServerTool(Cog):
     (Cog.HelpCommand(invites)
         .merge_headline(ja="招待ランキング")
         .set_description(ja="招待ランキングを表示します。", en=invites.description))
+
+    @commands.command(
+        aliases=("perms", "権限", "戦闘力"), fsparent=FSPARENT,
+        description="Displays the permissions held by the specified member."
+    )
+    async def permissions(self, ctx: commands.Context, *, member: discord.Member | None = None):
+        member = member or ctx.guild.default_role # type: ignore
+        permissions = getattr(member, "guild_permissions", getattr(
+            member, "permissions", None
+        ))
+
+        if permissions is None:
+            await ctx.reply(t(dict(
+                ja="見つかりませんでした。", en="Not found..."
+            ), ctx))
+        else:
+            await ctx.reply(embed=Cog.Embed(
+                title=t(
+                    {"ja": "{name}の権限一覧", "en": "{name}'s Permissions"},
+                    ctx, name=member.name # type: ignore
+                ),
+                description="\n".join(
+                    f"{EMOJIS['success']} {t(PERMISSION_TEXTS[name], ctx)}"
+                        if getattr(permissions, name, False)
+                        else f"{EMOJIS['error']} {t(PERMISSION_TEXTS[name], ctx)}"
+                    for name in PERMISSION_TEXTS
+                )
+            ))
+
+    (Cog.HelpCommand(permissions)
+        .merge_headline(ja="指定したユーザーが所有している権限を表示します。")
+        .set_description(ja="指定したユーザーが所有している権限を表示します。", en=permissions.description)
+        .add_arg("member", "Member", "Optional",
+            ja="""所有している権限を見たい対象のメンバーです。
+                指定しない場合は`@everyone`ロール(全員が持っている権限)となります。""",
+            en="""The members of the target group who want to see the privileges they possess.
+                If not specified, it will be the `@everyone` role (the authority that everyone has)."""))
 
 
 async def setup(bot: RT) -> None:
