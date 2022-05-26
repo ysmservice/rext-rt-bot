8aa35c76c739b36f927b5e547898c86457bff5ac
tuna2134 <masato.04.11.2007@gmail.com>
2022-05-26 13:43:07 +0900

    Fix: wave1

--- cogs/individual/__init__.py
+++ cogs/individual/__init__.py
@@ -403,16 +403,9 @@ class Individual(Cog):
         description="Show server information", fsparent=FSPARENT
     )
     @discord.app_commands.describe(target="server id")
-    async def serverinfo(self, ctx, target: int = None):
+    async def serverinfo(self, ctx, target: int | None = None):
         guild = await self.bot.search_guild(target)
-        embed = discord.Embed(
-            title=t(
-                {
-                    "ja": "{name}の情報",
-                    "en": "{name}'s information"
-                }, ctx, name=guild.name
-            ),
-        )
+        embed = Cog.Embed(title=t({"ja": "{name}の情報","en": "{name}'s information"}, ctx, name=guild.name))
         embed.add_field(
             name=t({"ja": "サーバー名", "en": "Server name"}, ctx),
             value=f"{guild.name} (`{guild.id}`)"
