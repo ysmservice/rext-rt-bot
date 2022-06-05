9331e73641d9e04385ee15b91f0921eb0cc6e924
tasuren <tasuren_0618@yahoo.co.jp>
2022-06-05 09:25:49 +0900

    update: status.pyをchannel_status.pyに名前を変更した。

--- cogs/server-management/channel_status.py
+++ cogs/server-management/channel_status.py
@@ -1,4 +1,4 @@
-# RT - Status
+# RT - Channel Status
 
 from discord.ext import commands
 import discord
@@ -10,32 +10,32 @@ from data import SET_ALIASES, DELETE_ALIASES, LIST_ALIASES, FORBIDDEN, CHANNEL_N
 from .__init__ import FSPARENT
 
 
-class Status(Cog):
+class ChannelStatus(Cog):
     def __init__(self, bot: RT):
         self.bot = bot
 
     @commands.group(
-        aliases=("stats", "sts", "ステータス", "す"), fsparent=FSPARENT,
+        aliases=("ch_stats", "csts", "チャンネルステータス", "ちす"), fsparent=FSPARENT,
         description="Displays information about the server on the channel."
     )
-    async def status(self, ctx: commands.Context):
+    async def channel_status(self, ctx: commands.Context):
         await self.group_index(ctx)
 
-    @status.command(
+    @channel_status.command(
         "set", aliases=SET_ALIASES,
         description="Set the status of the server to the channel."
     )
     async def set_(self, ctx: commands.Context):
         ...
 
-    @status.command(aliases=DELETE_ALIASES, description="Delete the setting of the status.")
+    @channel_status.command(aliases=DELETE_ALIASES, description="Delete the setting of the status.")
     async def delete(self, ctx: commands.Context):
         ...
 
-    @status.command("list", aliases=LIST_ALIASES, description="Displays list of settings.")
+    @channel_status.command("list", aliases=LIST_ALIASES, description="Displays list of settings.")
     async def list_(self, ctx: commands.Context):
         ...
 
 
 async def setup(bot: RT) -> None:
-    await bot.add_cog(Status(bot))
+    await bot.add_cog(ChannelStatus(bot))
