0706c14b51696115a19ac631269c4f837a2ec5e2
mc_fdc <Masato.04.11.2007@gmail.com>
2022-06-12 18:06:47 +0900

    Add: kick

--- cogs/server-management2/__init__.py
+++ cogs/server-management2/__init__.py
@@ -1,3 +1,37 @@
 # RT - Server Management 2
+import discord
+
+from core import Cog
+
 
 FSPARENT = "server-management2"
+
+
+class ServerManagement(Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        
+    @commands.command(description="Kick a user", fsparent=FSPARENT)
+    @discord.app_commands.describe(target="Target member", reason="Reason")
+    async def kick(self, ctx, target: discord.Member, reason: str=None):
+        await target.kick(reason=readon)
+        await ctx.send(f"👋 Kicked {target.mention}")
+        
+    Cog.HelpCommand(kick) \
+        .merge_headline(ja="ユーザーをkickします。") \
+        .set_description(
+            ja="ユーザーをkickします。",
+            en=kick.description
+        ) \
+        .add_args(
+            "target", "Member",
+            ja="対象とするユーザー",
+            en="Target member") \
+        .add_args(
+            "reason", "str",
+            ja="理由",
+            en="reason")
+        
+
+async def setup(bot):
+    await bot.add_cog(ServerManagement(bot))
