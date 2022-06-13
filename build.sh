560a42b0bda1113bc82446c31f0a8172d0f19f8c
mc_fdc <Masato.04.11.2007@gmail.com>
2022-06-13 22:38:58 +0900

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
+echo "Now setuping..."
 cp secret.json.template secret.json && cp data.json.template data.json
 python3 -m pip install -r requirements.txt
 git clone https://github.com/RextTeam/rt-lib
@@ -5,3 +6,4 @@ mv rt-lib rtlib
 cd ./rtlib && python3 -m pip install -r requirements.txt
 cd ./common && python3 make_key.py
 cd ../../ && mv rtlib/common/secret.key ./
+echo "\nAll setup is finish"
