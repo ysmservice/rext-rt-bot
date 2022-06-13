679d6e7b3da72cfdebf194b0e33466f428fa97a0
tasuren <tasuren_0618@yahoo.co.jp>
2022-06-14 06:19:13 +0900

    update: 色々コーディング規約に沿った。

--- cogs/server-management2/__init__.py
+++ cogs/server-management2/__init__.py
@@ -1,28 +1,29 @@
 # RT - Server Management 2
+
 import discord
 from discord.ext import commands
 
-from core import Cog
+from core import Cog, RT
 
 
 FSPARENT = "server-management2"
 
 
 class ServerManagement2(Cog):
-    def __init__(self, bot):
+    def __init__(self, bot: RT):
         self.bot = bot
-        
+
     @commands.command(description="Kick a user", fsparent=FSPARENT)
     @discord.app_commands.describe(target="Target member", reason="Reason")
-    async def kick(self, ctx, target: discord.Member, *, reason: str=None):
+    async def kick(self, ctx, target: discord.Member, *, reason: str = None):
         await target.kick(reason=reason)
         await ctx.reply(f"👋 Kicked {self.name_and_id(target)}")
-        
+
     Cog.HelpCommand(kick) \
         .merge_description("headline", ja="ユーザーをkickします。") \
         .add_arg("target", "Member", ja="対象とするユーザー", en="Target member") \
         .add_arg("reason", "str", ja="理由", en="reason")
-        
 
-async def setup(bot):
+
+async def setup(bot: RT) -> None:
     await bot.add_cog(ServerManagement2(bot))
