40b269e2a1f7d8bbec58e829bed9bb339970901d
mc_fdc <Masato.04.11.2007@gmail.com>
2022-06-13 20:28:22 +0900

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,6 +3,4 @@ git clone https://github.com/RextTeam/rt-lib
 mv rt-lib rtlib
 cd ./rtlib && python3 -m pip install -r requirements.txt
 cd ./common && python3 make_key.py
-mv secret.key ../
-cd ../ && mv secret.key ../
-cd ../ && mv secret.key ../
+cd ../../ && mv rtlib/common/secret.key ./
