54ac8133910c7e8fe2872fc4a3af8163f60cc5cb
mc_fdc <Masato.04.11.2007@gmail.com>
2022-06-14 08:41:38 +0000

    add: いろいろ

--- cogs/server-tool/globalchat.py
+++ cogs/server-tool/globalchat.py
@@ -1,20 +1,29 @@
 # RT - server-tool global
+from typing import AsyncIterator, Optional
+
 import discord
 from discord.ext import commands
 
-from core import Cog, RT, t, DatabaseManager, cursor
+from core import (
+    Cog,
+    RT,
+    t,
+    DatabaseManager,
+    cursor
+)
 
 
 class DataManager(DatabaseManager):
     def __init__(self, bot: RT):
         self.pool = bot.pool
+        self.bot = bot
 
     async def create_chat(self, name: str, channel: discord.TextChannel) -> bool:
         await cursor.execute(
             "SELECT * FROM GlobalChat WHERE name=%s",
             (name,)
         )
-        if (await cursor.fetchone()) is None:
+        if (await cursor.fetchone()) is not None:
             return False
         await cursor.execute(
             "INSERT INTO GlobalChat VALUES (%s, %s)",
@@ -22,18 +31,47 @@ class DataManager(DatabaseManager):
         )
         return True
 
+    async def connect(self, name: str, channel: discord.TextChannel) -> None:
+        await cursor.execute(
+            "INSERT INTO GlobalChat VALUES (%s, %s)",
+            (name, channel.id)
+        )
+
     async def prepare_table(self) -> None:
         await cursor.execute(
             """CREATE TABLE IF NOT EXISTS GlobalChat(
                 name TEXT, channelid BIGINT);"""
         )
 
-    async def check_exist(self, channel: discord.TextChannel) -> None:
+    async def check_exist(self, channel: discord.TextChannel) -> bool:
         await cursor.execute(
             "SELECT * FROM GlobalChat WHERE channelid=%s",
             (channel.id,)
         )
         return (await cursor.fetchone()) is not None
+
+    async def check_exist_gc(self, name: str) -> None:
+        await cursor.execute(
+            "SELECT * FROM GlobalChat WHERE name=%s",
+            (name,)
+        )
+        return (await cursor.fetchone()) is not None
+
+    async def get_all_channel(self, name: str) -> AsyncIterator[discord.TextChannel]:
+        await cursor.execute(
+            "SELECT * FROM GlobalChat WHERE name=%s",
+            (name,)
+        )
+        for row in (await cursor.fetchall()):
+            yield await self.bot.search_channel(row[1])
+
+    async def get_name(self, channel: discord.TextChannel) -> Optional[str]:
+        await cursor.execute(
+            "SELECT * FROM GlobalChat WHERE channelid=%s",
+            (channel.id,)
+        )
+        data = await cursor.fetchone()
+        return data[0] if data is not None else None
 
 
 class GlobalChat(Cog):
@@ -85,6 +123,47 @@ class GlobalChat(Cog):
                 ),
                 ctx
             ))
+
+    @globalchat.command(
+        description="Connect to global chat",
+        aliases=("join",)
+    )
+    async def connect(self, ctx, name: str = None):
+        if (await self.data.check_exist(ctx.channel)):
+            return await ctx.reply(t(
+                dict(
+                    en="You connected another one.",
+                    ja="もうすでにあなたは接続をしています。"
+                )
+                ,ctx
+            ))
+        name = "default" if name is None else name
+        if not (await self.data.check_exist_gc(name)):
+            return await ctx.reply(t(
+                dict(
+                    en="Not found",
+                    ja="見つかりませんでした。"
+                ),
+                ctx
+            ))
+        await self.data.connect(name, ctx.channel)
+        await ctx.reply(t(
+            dict(
+                en="Connected",
+                ja="接続しました。"
+            ),
+            ctx
+        ))
+
+    @Cog.listener("on_message")
+    async def on_message(self, message):
+        if message.author.bot:
+            return
+        if not (await self.data.check_exist(message.channel)):
+            return
+        name = await self.data.get_name(message.channel)
+        async for channel in self.data.get_all_channel(name):
+            print(channel.name)
 
 
 async def setup(bot: RT):
