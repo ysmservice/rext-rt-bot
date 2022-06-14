d88977cd98f4eb4604b672ba28978d6fd21044df
tunaxl <tunaxl@trsp>
2022-06-14 14:33:51 +0900

    fix: pep8

--- cogs/server-tool/globalchat.py
+++ cogs/server-tool/globalchat.py
@@ -8,7 +8,7 @@ from core import Cog, RT, t, DatabaseManager, cursor
 class DataManager(DatabaseManager):
     def __init__(self, bot: RT):
         self.pool = bot.pool
-    
+
     async def create_chat(self, name: str, channel: discord.TextChannel) -> bool:
         await cursor.execute(
             "SELECT * FROM GlobalChat WHERE name=%s",
@@ -21,14 +21,14 @@ class DataManager(DatabaseManager):
             (name, channel.id)
         )
         return True
-        
+
     async def prepare_table(self) -> None:
         await cursor.execute(
             """CREATE TABLE IF NOT EXISTS GlobalChat(
                 name TEXT, channelid BIGINT
             );"""
         )
-        
+
     async def check_exist(self, channel: discord.TextChannel) -> None:
         await cursor.execute(
             "SELECT * FROM GlobalChannel WHERE channelid=%s",
@@ -41,10 +41,10 @@ class GlobalChat(Cog):
     def __init__(self, bot: RT):
         self.bot = bot
         self.data = DataManager(bot)
-        
+
     async def cog_load(self):
         await self.data.prepare_table()
-        
+
     @commands.group(
         description="Setup global chat"
         aliases=("gc", "gchat")
@@ -52,7 +52,7 @@ class GlobalChat(Cog):
     async def globalchat(self, ctx):
         if ctx.invoked_subcommand is None:
             await ctx.reply(t(ctx, dict(en="Invaild command", ja="使い方が間違っているゾ")))
-            
+
     @globalchat.command(
         description="Create globalchat",
         aliases=("make", "add")
@@ -66,7 +66,7 @@ class GlobalChat(Cog):
             await ctx.reply(t(ctx, dict(en="Created", ja="作成しました。")))
         else:
             await ctx.reply(t(ctx, dict(en="Allready created", ja="すでに作成しているゾ")))
-    
+
 
 async def setup(bot: RT):
     await bot.add_cog(GlobalChat(bot))
