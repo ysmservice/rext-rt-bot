26d63184addb40a88240d3860e741270cec8ab3e
mc_fdc <Masato.04.11.2007@gmail.com>
2022-06-14 13:36:15 +0900

    add: ban

--- cogs/server-management2/__init__.py
+++ cogs/server-management2/__init__.py
@@ -18,10 +18,22 @@ class ServerManagement2(Cog):
     async def kick(self, ctx, target: discord.Member, *, reason: str = None):
         await target.kick(reason=reason)
         await ctx.reply(f"👋 Kicked {self.name_and_id(target)}")
+        
+    @commands.command(description="Ban a user", fsparent=FSPARENT)
+    @discord.app_commands.describe(target="Target user", reason="Reason")
+    async def ban(self, ctx, targetid: int, *, reason: str = None):
+        target = await self.bot.search_user(targetid)
+        await ctx.guild.ban(target, reason=reason)
+        await ctx.reply(f"👋 Baned {self.name_and_id(target)}")
 
     Cog.HelpCommand(kick) \
         .merge_description("headline", ja="ユーザーをkickします。") \
         .add_arg("target", "Member", ja="対象とするユーザー", en="Target member") \
+        .add_arg("reason", "str", ja="理由", en="reason")
+    
+    Cog.HelpCommand(ban) \
+        .merge_description("headline", ja="対象のユーザーをbanします。") \
+        .add_arg("target", "intger", ja="対象とするユーザーID", en="Target user id") \
         .add_arg("reason", "str", ja="理由", en="reason")
 
 
