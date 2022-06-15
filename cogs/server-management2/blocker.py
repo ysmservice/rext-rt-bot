98df54c21f644ed507b3913b5ff2d51d49a41a9f
Yaakiyu <62779277+yaakiyu@users.noreply.github.com>
2022-06-15 16:16:08 +0900

    update: blocker75%完成

--- cogs/server-management2/blocker.py
+++ cogs/server-management2/blocker.py
@@ -9,16 +9,20 @@ import discord
 from collections import defaultdict
 
 from rtlib.common.json import loads, dumps
+
+from data import ADD_ALIASES, REMOVE_ALIASES
+
 from core import Cog, DataBaseManager, cursor, RT
+from core.views import EmbedPage
 
 from .__init__ import FSPARENT
 
 
 class DataManager(DataBaseManager):
 
+    MODES = ("emoji", "stamp", "reaction")
     MODES_CL: TypeAlias = Literal["Emoji", "Stamp", "Reaction"]
     MODES_L: TypeAlias = Literal["emoji", "stamp", "reaction", "all"]
-    MODES = ("emoji", "stamp", "reaction")
 
     def __init__(self, bot: RT):
         self.bot = bot
@@ -37,10 +41,10 @@ class DataManager(DataBaseManager):
                 for row in rows:
                     self.onoff_cache[row[0]][table] = row[1]
 
-    async def toggle(self, guild_id: int, table: MODES_L) -> bool | tuple[bool]:
+    async def toggle(self, guild_id: int, table: MODES_L, **_) -> bool | tuple[bool]:
         "設定のオンオフを切り替えます。"
         if table == "all":
-            return (await self.toggle(guild_id, mo) for mo in self.MODES)
+            return (await self.toggle(guild_id, mo, cursor=cursor) for mo in self.MODES)
 
         table = table.capitalize()
 
@@ -60,17 +64,17 @@ class DataManager(DataBaseManager):
         self.onoff_cache[guild_id][table] = onoff
         return onoff
 
-    async def add_role(self, guild_id: int, table: MODES_L, role_id: int) -> None:
+    async def add_role(self, guild_id: int, table: MODES_L, role_id: int, **_) -> None:
         "ロールを追加します。"
         if table == "all":
             for mo in ("emoji", "stamp", "reaction"):
-                await self.add_role(guild_id, mo, role_id)
+                await self.add_role(guild_id, mo, role_id, cursor=cursor)
             return
 
         table = table.capitalize()
 
-        if role_id in (now := self.get_now_roles(guild_id, table)):
-            raise ValueError("既に登録しています。")
+        if role_id in (now := self.get_now_roles(guild_id, table, cursor=cursor)) or len(now) > 15:
+            raise ValueError("既に登録しているまたはこれ以上設定できません。")
 
         await cursor.execute(
             f"""INSERT INTO {table}Blocker VALUES (%s, %s, %s, %s)
@@ -80,22 +84,22 @@ class DataManager(DataBaseManager):
         )
 
     async def remove_role(
-        self, guild_id: int, table: MODES_L, role_id: int
+        self, guild_id: int, table: MODES_L, role_id: int, **_
     ) -> list[MODES_L] | None:
         """ロールを削除します。未設定orそのロールが設定されていない場合はValueErrorを送出します。
         table引数が`all`だった場合には削除に成功したテーブルのリストを返します。"""
         if table == "all":
             succeed = []
-            for mo in ("emoji", "stamp", "reaction"):
-                try: await self.remove_role(guild_id, mo, role_id)
+            for mo in self.MODES:
+                try: await self.remove_role(guild_id, mo, role_id, cursor=cursor)
                 except ValueError: pass
                 else: succeed.append(mo)
             return succeed
 
         table = table.capitalize()
 
-        if not (now := self.get_now_roles(guild_id, table)) or guild_id not in now:
-            raise ValueError("未設定もしくはロールが設定されていません。")
+        if not (now := self.get_now_roles(guild_id, table, cursor=cursor)) or role_id not in now:
+            raise ValueError("そのロールは設定されていません。")
 
         now.remove(guild_id)
         await cursor.execute(
@@ -104,7 +108,7 @@ class DataManager(DataBaseManager):
         )
 
     async def get_settings(
-        self, guild_id: int, mode: MODES_CL, get_type: str | None = None
+        self, guild_id: int, mode: MODES_CL, get_type: str | None = None, **_
     ) -> tuple:
         "設定を取得します。get_typeが指定されていなければ全て返します。"
         await cursor.execute(
@@ -114,24 +118,25 @@ class DataManager(DataBaseManager):
         )
         return await cursor.fetchone()
 
-    async def get_now_roles(self, guild_id: int, mode: MODES_CL) -> list:
+    async def get_now_roles(self, guild_id: int, mode: MODES_CL, **_) -> list:
         "現在のロール設定を取得します。この機能が設定されていない場合は[]です。"
-        now_roles = self.get_settings(guild_id, mode, "Roles")
+        now_roles = self.get_settings(guild_id, mode, "Roles", cursor=cursor)
         return loads(now_roles[0][0]) if now_roles else []
 
 
 class BlockerDeleteEventContect(Cog.EventContext):
     "ブロッカー機能で何かを削除したときのベースイベントコンテキストです。"
+
     channel: discord.TextChannel | None
     message: discord.Message | None
     member: discord.Member | None
-
 class BlockerDeleteEmojiEventContect(BlockerDeleteEventContect):
     "ブロッカー機能で絵文字を削除したときのイベントコンテキストです。"
-    emoji: discord.Emoji | None
 
+    emoji: discord.Emoji | None
 class BlockerDeleteStampEventContect(BlockerDeleteEventContect):
     "ブロッカー機能でスタンプを削除したときのイベントコンテキストです。"
+
     stamp: discord.Sticker | discord.StickerItem | None
 
 
@@ -140,30 +145,88 @@ class Blocker(Cog):
         self.bot = bot
         self.data = DataManager(bot)
 
+    MODES_JA = {
+        "stamp": "スタンプ",
+        "emoji": "絵文字",
+        "reaction": "リアクション"
+    }
+
     async def cog_load(self) -> None:
         await self.data.prepare_table()
 
-    @commands.group(description="Setting blocker.")
+    @commands.group(aliases=("block", "ブロッカー"), description="Setting blocker.", fsparent=FSPARENT)
     @commands.has_guild_permissions(administrator=True)
     @commands.guild_only()
     async def blocker(self, ctx):
         if ctx.invoked_subcommand: return
-        await ctx.send("view blocker settings")
 
-    @blocker.command(description="Toggles blocker.")
+        embeds = []
+        for mo in self.data.MODES:
+            se = await self.data.get_settings(ctx.guild.id, mo, "Blocking, Roles")
+            # 今後例外機能を追加する場合はExceptionsを処理する。
+
+            embed = Cog.Embed(t(dict(
+                ja=f"{MODES_JA[mo]}ブロッカーの設定", en=f"Settings of {mo} blocker"
+            ), ctx), description=t(dict(
+                ja="設定されていません。", en="Not setting."
+            ), ctx) if not se else t(dict(
+                ja="オン" if se[0][0] else "オフ", en="ON" if se[0][0] else "OFF"
+            ), ctx))
+
+            if not se or se[0][0]:
+                continue
+            embed.add_field(
+                name=t({"ja": "設定済みロール", "en": "Setting Roles"}, ctx),
+                value=", ".join(ctx.guild.get_role(r).mention for r in se[0][1]) or "..."
+            )
+            embeds.append(embed)
+
+        view = EmbedPage(embeds)
+        m = await ctx.send(embed=embeds[0], view=view)
+        view.set_message(m)
+
+    @blocker.command(description="Toggle blocker.")
     @app_commands.describe(mode="Blocking type")
     async def toggle(self, ctx, mode: DataManager.MODES_L):
-        await ctx.send("Ok")
+        result = await self.data.toggle(ctx.guild.id, mode)
+        await ctx.send(t(dict(
+            ja=f"{self.MODES_JA[mode]}を{'有効化' if result else '無効化'}しました。",
+            en=f"{'Enabled' if result else 'Disabled'} {mode} blocker."
+        ), ctx))
 
     @blocker.group(description="Set blocking roles.")
     async def role(self, ctx):
-        if not ctx.invoked_subcommand:
-            await ctx.send("wrong usage")
+        pass
 
-    @role.command(description="Add Blocking Roles.")
+    @role.command(aliases=ADD_ALIASES, description="Add Blocking Roles.")
     @app_commands.describe(mode="Blocking type", role="Adding role")
     async def add(self, ctx, mode: DataManager.MODES_L, role: discord.Role):
+        try:
+            self.data.add_role(ctx.guild.id, mode, role.id)
+        except ValueError as e:
+            return await ctx.send(t(dict(
+                ja=e.args[0], en="Already set this role or you can't set more."
+            )))
         await ctx.send("Ok")
+
+    @role.command(aliases=REMOVE_ALIASES, description="Remove Blocking Roles.")
+    @app_commands.describe(mode="Blocking type", role="Removing role")
+    async def remove(self, ctx, mode: DataManager.MODES_L, role: discord.Role):
+        try:
+            result = self.data.remove_role(ctx.guild.id, mode, role.id)
+        except ValueError as e:
+            return await ctx.send(t(dict(
+                ja=e.args[0], en="You didn't set the role."
+            )))
+        if result:
+            await ctx.send(t(dict(
+                ja=f"{'ブロッカー, '.join(self.MODES_JA[mode] for mode in result)}ブロッカーのそのロールの設定を解除しました。"
+                   if len(result) != 0 else "どのブロッカーもそのロールは設定していませんでした。",
+                en=f"Removed that role setting of {' blocker, '.join(self.MODES_JA[mode] for mode in result)}blocker."
+                   if len(result) != 0 else "Any type of blocker didn't set the role."
+            )))
+        else:
+            await ctx.send("Ok")
 
     @commands.Cog.listener()
     async def on_message(self, message):
