4beca290a61eec53f742f898e903c9c282289a06
Yaakiyu <62779277+yaakiyu@users.noreply.github.com>
2022-06-15 12:12:37 +0900

    update;: blocker6割完成。

--- cogs/server-management2/blocker.py
+++ cogs/server-management2/blocker.py
@@ -6,8 +6,9 @@ from discord.ext import commands
 from discord import app_commands
 import discord
 
-from rtlib.common.json import loads, dumps
+from collections import defaultdict
 
+from rtlib.common.json import loads, dumps
 from core import Cog, DataBaseManager, cursor, RT
 
 from .__init__ import FSPARENT
@@ -15,45 +16,90 @@ from .__init__ import FSPARENT
 
 class DataManager(DataBaseManager):
 
-    MODES = ("Emoji", "Stamp", "Reaction")
     MODES_CL: TypeAlias = Literal["Emoji", "Stamp", "Reaction"]
-    MODES_L: TypeAlias = Literal["emoji", "stamp", "reaction"]
+    MODES_L: TypeAlias = Literal["emoji", "stamp", "reaction", "all"]
+    MODES = ("emoji", "stamp", "reaction")
 
     def __init__(self, bot: RT):
         self.bot = bot
+        # 設定のonoffだけキャッシュに入れておく。
+        self.onoff_cache = defaultdict(dict)
 
     async def prepare_table(self) -> None:
         "テーブルを準備します。"
-        for table in self.MODES:
+        for table in ("Emoji", "Stamp", "Reaction"):
             await cursor.execute(
                 f"""CREATE TABLE IF NOT EXISTS {table}Blocker(
                     GuildId BIGINT PRIMARY KEY NOT NULL, Blocking BOOLEAN,
                     Roles JSON, Exceptions JSON
                 );""")
+            async for rows in self.fetchstep(cursor, f"SELECT GuildId, Blocking FROM {table}Blocker;"):
+                for row in rows:
+                    self.onoff_cache[row[0]][table] = row[1]
 
-    async def add_role(self, guild_id: int, mode: MODES_L, role_id: int) -> None:
+    async def toggle(self, guild_id: int, table: MODES_L) -> bool | tuple[bool]:
+        "設定のオンオフを切り替えます。"
+        if table == "all":
+            return (await self.toggle(guild_id, mo) for mo in self.MODES)
+
+        table = table.capitalize()
+
+        if guild_id not in self.onoff_cache:
+            await cursor.execute(
+                f"INSERT INTO {table}Blocker VALUES (%s, true, %s, %s)",
+                (guild_id, "[]", "{}")
+            )
+            self.onoff_cache[guild_id][table] = True
+            return True
+
+        onoff = not self.onoff_cache[guild_id][table]
+        await cursor.execute(
+            f"UPDATE {table}Blocker Blocking = %s WHERE GuildId = %s",
+            (onoff, guild_id)
+        )
+        self.onoff_cache[guild_id][table] = onoff
+        return onoff
+
+    async def add_role(self, guild_id: int, table: MODES_L, role_id: int) -> None:
         "ロールを追加します。"
-        mode = mode.capitalize()
-        now = self.get_now_roles(guild_id, mode)
+        if table == "all":
+            for mo in ("emoji", "stamp", "reaction"):
+                await self.add_role(guild_id, mo, role_id)
+            return
+
+        table = table.capitalize()
+
+        if role_id in (now := self.get_now_roles(guild_id, table)):
+            raise ValueError("既に登録しています。")
 
         await cursor.execute(
-            f"""INSERT INTO {mode}Blocker VALUES (%s, %s, %s, %s)
+            f"""INSERT INTO {table}Blocker VALUES (%s, %s, %s, %s)
                 ON DUPLICATE KEY UPDATE Roles = %s;""",
-            (
-                guild_id, False, dumps([role_id]), "[]",
-                dumps(now + [role_id])
-            )
+            (guild_id, False, dumps([role_id]), "[]",
+                dumps(now + [role_id]))
         )
 
-    async def remove_role(self, guild_id: int, mode: MODES_L, role_id: int) -> None:
-        "ロールを削除します。未設定orそのロールが設定されていない場合はValueErrorを送出します。"
-        mode = mode.capitalize()
-        if not (now := self.get_settings(guild_id, mode)) or guild_id not in now:
-            raise ValueError("未設定もしくはロールが設定されていません。")
-        now.remove(guild_id)
+    async def remove_role(
+        self, guild_id: int, table: MODES_L, role_id: int
+    ) -> list[MODES_L] | None:
+        """ロールを削除します。未設定orそのロールが設定されていない場合はValueErrorを送出します。
+        table引数が`all`だった場合には削除に成功したテーブルのリストを返します。"""
+        if table == "all":
+            succeed = []
+            for mo in ("emoji", "stamp", "reaction"):
+                try: await self.remove_role(guild_id, mo, role_id)
+                except ValueError: pass
+                else: succeed.append(mo)
+            return succeed
 
+        table = table.capitalize()
+
+        if not (now := self.get_now_roles(guild_id, table)) or guild_id not in now:
+            raise ValueError("未設定もしくはロールが設定されていません。")
+
+        now.remove(guild_id)
         await cursor.execute(
-            f"""UPDATE {mode}Blocker SET Roles = %s WHERE GuildId = %s;""",
+            f"""UPDATE {table}Blocker SET Roles = %s WHERE GuildId = %s;""",
             (dumps(now), guild_id)
         )
 
@@ -62,7 +108,8 @@ class DataManager(DataBaseManager):
     ) -> tuple:
         "設定を取得します。get_typeが指定されていなければ全て返します。"
         await cursor.execute(
-            f"SELECT {get_type if get_type else '*'} FROM {mode} WHERE GuildId = %s;",
+            f"""SELECT {get_type if get_type else '*'} FROM {mode}Blocker
+                WHERE GuildId = %s LIMIT 1;""",
             (guild_id,)
         )
         return await cursor.fetchone()
@@ -71,6 +118,21 @@ class DataManager(DataBaseManager):
         "現在のロール設定を取得します。この機能が設定されていない場合は[]です。"
         now_roles = self.get_settings(guild_id, mode, "Roles")
         return loads(now_roles[0][0]) if now_roles else []
+
+
+class BlockerDeleteEventContect(Cog.EventContext):
+    "ブロッカー機能で何かを削除したときのベースイベントコンテキストです。"
+    channel: discord.TextChannel | None
+    message: discord.Message | None
+    member: discord.Member | None
+
+class BlockerDeleteEmojiEventContect(BlockerDeleteEventContect):
+    "ブロッカー機能で絵文字を削除したときのイベントコンテキストです。"
+    emoji: discord.Emoji | None
+
+class BlockerDeleteStampEventContect(BlockerDeleteEventContect):
+    "ブロッカー機能でスタンプを削除したときのイベントコンテキストです。"
+    stamp: discord.Sticker | discord.StickerItem | None
 
 
 class Blocker(Cog):
@@ -88,9 +150,9 @@ class Blocker(Cog):
         if ctx.invoked_subcommand: return
         await ctx.send("view blocker settings")
 
-    @blocker.command("toggle", description="Toggles blocker.")
+    @blocker.command(description="Toggles blocker.")
     @app_commands.describe(mode="Blocking type")
-    async def _toggle(self, ctx, mode: DataManager.MODES_L):
+    async def toggle(self, ctx, mode: DataManager.MODES_L):
         await ctx.send("Ok")
 
     @blocker.group(description="Set blocking roles.")
@@ -99,8 +161,16 @@ class Blocker(Cog):
             await ctx.send("wrong usage")
 
     @role.command(description="Add Blocking Roles.")
+    @app_commands.describe(mode="Blocking type", role="Adding role")
     async def add(self, ctx, mode: DataManager.MODES_L, role: discord.Role):
         await ctx.send("Ok")
+
+    @commands.Cog.listener()
+    async def on_message(self, message):
+        if not ctx.guild or message.author.bot or \
+                message.guild.id not in self.data.onoff_cache:
+            return
+        pass
 
 
 async def setup(bot: RT):
