4b885e763eb55c640a4b793052acf41adba2e176
mc_fdc <Masato.04.11.2007@gmail.com>
2022-07-01 16:44:41 +0900

    add: shard

--- main.py
+++ main.py
@@ -5,19 +5,24 @@ from asyncio import run
 from discord import Intents, Status, Game, AllowedMentions
 
 from core.bot import RT
-from data import SECRET
+from data import SECRET, SHARD, NormalData
 
 try: from uvloop import install
 except ModuleNotFoundError: ...
 else: install()
 
+    
+kwargs = {}
+if SHARD:
+    if NormalData["shard"] != "auto":
+        kwargs["shard_ids"] = NormalData["shard"]
 
 intents = Intents.default()
 intents.message_content = True
 intents.members = True
 bot = RT(
     allowed_mentions=AllowedMentions(everyone=False), intents=intents,
-    status=Status.dnd, activity=Game("起動")
+    status=Status.dnd, activity=Game("起動"), **kwargs
 )
 bot.print("Now loading...")
 
