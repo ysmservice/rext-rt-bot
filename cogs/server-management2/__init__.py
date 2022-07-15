008aff7f59b7e15ace41c45f0f42048b5c6b4597
mc_fdc <Masato.04.11.2007@gmail.com>
2022-07-15 16:34:17 +0900

    Update __init__.py

--- cogs/server-management2/__init__.py
+++ cogs/server-management2/__init__.py
@@ -30,8 +30,8 @@ class ServerManagement2(Cog):
             raise Cog.BadRequest({"ja": "10人以下までしかできません。", "en": "You can only specify up to 10 people."})
         await asyncio.gather(*(ctx.guild.ban(
             discord.Object(target_id, reason=reason)
-            for target_id in target_ids)
-        )
+            for target_id in target_ids
+        ))
         await ctx.reply("\n".join(f"👋 Baned `{target_id}`" for target_id in target_ids))
 
     @commands.command(
